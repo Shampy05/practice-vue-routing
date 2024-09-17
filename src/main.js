ab184c8d3523993a0b4a5ad98c435a5710e3ce88
Pax Sharma <sharmapranshu317@gmail.com>
2024-09-17 15:55:32 +0100

    redirect and catch all routes

--- src/main.js
+++ src/main.js
@@ -5,20 +5,30 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue'
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
     routes: [
         {
+            path: '/',
+            redirect: '/teams',
+        },
+        {
             path: '/teams',
-            component: TeamsList
+            component: TeamsList,
         },
         {
             path: '/users',
-            component: UsersList
+            component: UsersList,
         },
         {
             path: '/teams/:teamId',
-            component: TeamMembers
+            component: TeamMembers,
+            props: true
+        },
+        {
+            path: '/:notFound(.*)',
+            component: NotFound,
         }
     ],
     history: createWebHistory()
